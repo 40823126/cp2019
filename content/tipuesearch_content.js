7e4b9d43062a793800587b9e169e8d9a1c9088c2
40823131 <40823131@gm.nfu.edu.tw>
2019-10-22 16:37:50 +0800

    dart

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '\n', 'tags': '', 'url': 'About.html'}, {'title': 'image', 'text': '', 'tags': '', 'url': 'image.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': "\n gist: https://gist.github.com/40823126/df10e6e89b94562fbe62a85f7433451e \n https://mde.tw/dartpad/?id=df10e6e89b94562fbe62a85f7433451e \n void main() {\n  for (int i = 0; i < 5; i++) {\n    print('hello ${i + 1}');\n  }\n} \n", 'tags': '', 'url': 'About.html'}, {'title': 'image', 'text': '', 'tags': '', 'url': 'image.html'}]};
